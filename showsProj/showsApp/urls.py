f06e098c2f42b9f4f5910f011fe372ed5f837e9e
Subhodeep Kumar Singh <subhodeep2791999@gmail.com>
2024-09-01 19:55:28 +0530

    CRUD done

--- showsProj/showsApp/urls.py
+++ showsProj/showsApp/urls.py
@@ -3,5 +3,7 @@ from showsApp import views
 
 urlpatterns = [
     path("", views.index),
-    path("add/",views.addShow)
+    path("add/",views.addShow),
+    path("delete/<int:id>/",views.deleteShow),
+    path("update/<int:id>/",views.updateShow)
 ]
--- showsProj/showsApp/views.py
+++ showsProj/showsApp/views.py
@@ -11,10 +11,25 @@ def index(request):
     return render(request,"showsApp/index.html",context={"shows":shows})
 
 def addShow(request):
+    form=ShowsForm()
     if request.method=="POST":
         form=ShowsForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect("/")
-    form=ShowsForm()
+            return redirect("/")
     return render(request,"showsApp/addShow.html",{"form":form})
+
+def deleteShow(request,id):
+    show=Shows.objects.get(id=id)
+    show.delete()
+    return redirect("/")
+
+def updateShow(request,id):
+    show=Shows.objects.get(id=id)
+    form=ShowsForm(instance=show)
+    if request.method=="POST":
+        form=ShowsForm(request.POST,instance=show)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    return render(request,"showsApp/updateShow.html",{"form":form})
